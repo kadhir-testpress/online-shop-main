5037b776e23bd7de6d65be58d1e8464308a0b03a
kadhir <kadhir@kadhir.com>
2024-02-21 18:55:06 +0530

    List the Products

--- myshop/urls.py
+++ myshop/urls.py
@@ -18,4 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls', namespace='shop')),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
