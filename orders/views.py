f50ba372d3bf82cfa938ef16dac78ff0e7ab11cb
kadhir <kadhir@kadhir.com>
2024-02-23 11:12:46 +0530

    Add Celery Configuration for  Asynchronous Task for sending email after placing a order

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
-
+from .tasks import order_created
 # Create your views here.
 def order_create(request):
     cart = Cart(request)
@@ -16,6 +16,7 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
