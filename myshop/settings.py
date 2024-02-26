7afd9e2cfd40c56890d44b724965c48cc15576c7
kadhir <kadhir@kadhir.com>
2024-02-26 18:52:29 +0530

    Implement English and Spanish Support with django-parler

--- myshop/settings.py
+++ myshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'payment.apps.PaymentConfig',
     'coupons.apps.CouponsConfig',
     'rosetta',
+    'parler',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,17 @@ LANGUAGES = (
  ('en', _('English')),
  ('es', _('Spanish')),
 )
+
+PARLER_LANGUAGES = {
+    None: (
+        {'code':'en'},
+        {'code':'es'},
+    ),
+    'default':{
+        'fallback': 'en',
+        'hide_untranslated': False
+    }
+}
 
 LOCALE_PATHS = (
  os.path.join(BASE_DIR, 'locale/'),
